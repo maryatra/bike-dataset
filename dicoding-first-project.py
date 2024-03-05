fb73ecd2791968bb0dc7841ff22ee5f3e2f5bd02
maryatra <144456569+maryatra@users.noreply.github.com>
2024-03-06 00:24:51 +0700

    Update dicoding-first-project.py

--- dicoding-first-project.py
+++ dicoding-first-project.py
@@ -39,10 +39,14 @@ def assessing_data(data_df):
     for col in data_df.columns:
         st.write(f"Kolom '{col}': {data_df[col].nunique()} nilai unik")
 
+assessing_data(data_df)
+
+# Fungsi untuk melakukan pembersihan data
 def clean_data(data_df):
     data_df['season'] = data_df['season'].map({1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
     return data_df
 
+# Fungsi untuk menampilkan data wrangling
 def data_wrangling(data_df):
     st.subheader("Data Wrangling")
     
@@ -57,23 +61,6 @@ def data_wrangling(data_df):
     st.subheader("Cleaning Data")
     st.write("Mengubah nilai dari parameter 'season' menjadi nama musim")
     cleaned_data_df = clean_data(data_df.copy())
-    st.write(cleaned_data_df)
-
-# Fungsi untuk menjawab pertanyaan EDA
-def data_wrangling(data_df):
-    st.subheader("Data Wrangling")
-    
-    st.subheader("Gathering Data")
-    st.write("Berikut merupakan dataset dari Bike Sharing pada skala hari")
-    st.write(data_df)
-
-    st.subheader("Assessing Data")
-    st.write("Melakukan pemeriksaan parameter statistik menggunakan metode describe()")
-    st.write(data_df.describe())
-
-    st.subheader("Cleaning Data")
-    st.write("Mengubah nilai dari parameter 'season' menjadi nama musim")
-    cleaned_data_df = clean_data(data_df)
     st.write(cleaned_data_df)
 
 # Fungsi untuk menjawab pertanyaan EDA
@@ -92,9 +79,8 @@ def eda_questions(data_df):
     st.write(data_musim)
     st.write('Grafik')
     data_musim['total'] = data_musim['casual'] + data_musim['registered']
-    fig, ax = plt.subplots()
-    sns.barplot(data=data_musim, x="season", y="total", hue="weathersit", errorbar=None, ax=ax)
-    st.pyplot(fig)
+    sns.barplot(data=data_musim, x="season", y="total", hue="weathersit", errorbar=None)
+    st.pyplot()
 
     st.subheader('Pertanyaan 2')
     st.write("Pengaruh hari kerja/akhir pekan terhadap peminjaman untuk setiap musim?")
@@ -109,12 +95,11 @@ def eda_questions(data_df):
     st.write(data_holiday.corr())
     st.write('Grafik')
     data_holiday['total'] = data_holiday['casual'] + data_holiday['registered']
-    fig, ax = plt.subplots()
-    sns.scatterplot(data=data_holiday, x='season', y='casual', ax=ax)
-    sns.scatterplot(data=data_holiday, x='season', y='registered', ax=ax)
-    ax.set_ylim(150, 190)
-    st.pyplot(fig)
-    
+    sns.scatterplot(data=data_holiday, x='season', y='casual')
+    sns.scatterplot(data=data_holiday, x='season', y='registered')
+    plt.ylim(150, 190)
+    st.pyplot()
+
 def main():
     st.title('Analisis Data Bike Sharing')
     data_df = load_data()

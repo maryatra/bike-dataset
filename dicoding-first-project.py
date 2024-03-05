7085651a68d20f616bcade369cfcbff27aaba39c
maryatra <144456569+maryatra@users.noreply.github.com>
2024-03-06 00:55:25 +0700

    Update dicoding-first-project.py

--- dicoding-first-project.py
+++ dicoding-first-project.py
@@ -14,6 +14,23 @@ def clean_data(data_df):
     data_df['season'] = data_df['season'].map({1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
     return data_df
 
+# Fungsi untuk menampilkan data wrangling
+def data_wrangling(data_df):
+    st.subheader("Data Wrangling")
+    
+    st.subheader("Gathering Data")
+    st.write("Berikut merupakan dataset dari Bike Sharing pada skala hari")
+    st.write(data_df)
+
+    st.subheader("Assessing Data")
+    st.write("Melakukan pemeriksaan parameter statistik menggunakan metode describe()")
+    st.write(data_df.describe())
+
+    st.subheader("Cleaning Data")
+    st.write("Mengubah nilai dari parameter 'season' menjadi nama musim")
+    cleaned_data_df = clean_data(data_df.copy())
+    st.write(cleaned_data_df)
+
 # Fungsi untuk menjawab pertanyaan EDA
 def eda_questions(data_df):
     st.subheader('Exploratory Data Analysis (EDA)')
@@ -30,9 +47,8 @@ def eda_questions(data_df):
     st.write(data_musim)
     st.write('Grafik')
     data_musim['total'] = data_musim['casual'] + data_musim['registered']
-    fig, ax = plt.subplots()
-    sns.barplot(data=data_musim, x="season", y="total", hue="weathersit", errorbar=None, ax=ax)
-    st.pyplot(fig)
+    sns.barplot(data=data_musim, x="season", y="total", hue="weathersit", errorbar=None)
+    st.pyplot()
 
     st.subheader('Pertanyaan 2')
     st.write("Pengaruh hari kerja/ akhir pekan terhadap peminjaman untuk setiap musim?")
@@ -43,24 +59,26 @@ def eda_questions(data_df):
         "registered": "nunique",
     })
     st.write(data_holiday)
+    st.write('Mengidentifikasi relationship menggunakan correlation')
+    st.write(data_holiday.corr())
     st.write('Grafik')
     data_holiday['total'] = data_holiday['casual'] + data_holiday['registered']
-    fig, ax = plt.subplots()
-    sns.scatterplot(data=data_holiday, x='season', y='casual', ax=ax)
-    sns.scatterplot(data=data_holiday, x='season', y='registered', ax=ax)
-    ax.set_ylim(150, 190)
-    st.pyplot(fig)
-
+    sns.scatterplot(data=data_holiday, x='season', y='casual')
+    sns.scatterplot(data=data_holiday, x='season', y='registered')
+    plt.ylim(150, 190)
+    st.pyplot()
 
 def main():
     st.title('Analisis Data Bike Sharing')
     data_df = load_data()
     
-    menu = ["Exploratory Data Analysis (EDA)"]
+    menu = ["Data Wrangling", "Exploratory Data Analysis (EDA)"]
     choice = st.sidebar.selectbox("Pilihan Menu", menu)
 
-    if choice == "Exploratory Data Analysis (EDA)":
+    if choice == "Data Wrangling":
+        data_wrangling(data_df)
+    elif choice == "Exploratory Data Analysis (EDA)":
         eda_questions(data_df)
 
 if __name__ == "__main__":
-    main()
+    main() 

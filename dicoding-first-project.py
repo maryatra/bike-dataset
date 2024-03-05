067af601515190713d212f97e004786323b696fa
maryatra <144456569+maryatra@users.noreply.github.com>
2024-03-06 00:38:40 +0700

    Update dicoding-first-project.py

--- dicoding-first-project.py
+++ dicoding-first-project.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Fungsi untuk mengimpor data
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data():
     data_df = pd.read_csv('day.csv')
     return data_df
